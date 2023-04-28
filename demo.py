33406bdaf75a85b8c5e4b9879e9f0dbb646bdb24
Haosen He <s1760764005@outlook.com>
2023-04-28 02:13:51 -0700

    basic payroll processor completed

--- demo.py
+++ demo.py
@@ -1,23 +1,38 @@
-# Version 0.0.1
+# Version 0.0.3
 # April 20, 2023
+import pandas as pd # For importing, manipulating, and exporting data
 import re # Python regular expression support
-from datetime import datetime # Python datetime conversion support
+import datetime # Python datetime conversion support
 from concurrent.futures import process # Asynchronous callable execution
+from dateutil.easter import *
+import holidays
 from tkinter import * # For user interface
 from tkinter.filedialog import askopenfile, askopenfilename, askdirectory,asksaveasfile # UI-file system interaction
-import pandas as pd # For importing, manipulating, and exporting data
+from copy import deepcopy
+from collections import namedtuple
+from helpers import *
+from copy import deepcopy
 #######################################
 #Backend
+Range = namedtuple('Range', ['start', 'end'])
 df = pd.DataFrame()
-rates = pd.DataFrame()
+manager_rates = pd.DataFrame()
+non_manager_rates = pd.DataFrame()
+
+df_aug = pd.DataFrame()
+non_mgr_payroll = pd.DataFrame()
+mgr_payroll = pd.DataFrame()
+PAY_PERIOD = ''
 
 def open_shift_file():
+    '''
+    Done
+    '''
     global df
-    global rates
     filepath =''
     read_shift_lab.configure(text='')
     try:
-        processed_lab.config(text="")
+        shift_processed_lab.config(text="")
         save_lab.config(text="")
         filepath = askopenfilename()
         df=pd.read_excel(filepath)
@@ -33,56 +48,57 @@ def open_shift_file():
                     'Updated Check-Out Time',
                     'Staff Worked Duration',
                     'Staff Worked Duration (Minutes)']]
-            read_shift_lab.configure(text='Shift record loaded successfully.')
+            read_shift_lab.configure(text='Shift Record Loaded Successfully.')
         except:
-            read_shift_lab.configure(text='The file does not contain all columns needed.')
+            read_shift_lab.configure(text='The File Does Not Contain All Columns Needed.')
     except:
-        if filepath!='':
-            read_shift_lab.configure(text='The file must be an excel.')
+        if filepath != '':
+            read_shift_lab.configure(text='The File Must Be An Excel.')
 
 def open_rates_file():
-    global df
-    global rates
+    '''
+    Done
+    '''
+    global manager_rates, non_manager_rates
     filepath =''
     read_wage_lab.configure(text='')
     try:
-        processed_lab.config(text='')
+        shift_processed_lab.config(text='')
         save_lab.config(text='')
         filepath = askopenfilename()
-        rates=pd.read_excel(filepath)
-        try:    
-            rates = rates[['Service 1 Description (Code)', 
-                    'Service Provider','Check-In Date',
-                    'Check-In Time',
-                    'Updated Check-In Date',
-                    'Updated Check-In Time',
-                    'Check-Out Date',
-                    'Check-Out Time',
-                    'Updated Check-Out Date',
-                    'Updated Check-Out Time',
-                    'Staff Worked Duration',
-                    'Staff Worked Duration (Minutes)']]
-            read_wage_lab.configure(text='Shift record loaded successfully.')
+        # Read "Manger Rates" sheet
+        manager_rates = pd.read_excel(filepath, sheet_name="Manager Rates")
+        # Read "Non-manager Rates" sheet
+        non_manager_rates = pd.read_excel(filepath, sheet_name="Non-manager Rates")
+        try:
+            non_manager_rates = non_manager_rates[['Shift Code', 'Regular Hourly Wage', 'BOT Hourly Wage', 'Accrue Rate']]
+            manager_rates = manager_rates[['Name', 'Non-exempt Hourly Wage', 'Exempt Weekly Wage', 'Exempt Biweekly Wage', 'Accrue Rate']]
+            read_wage_lab.configure(text='Billing & Wage Info Loaded Successfully.')
         except:
-            read_wage_lab.configure(text='The file does not contain all columns needed.')
+            read_wage_lab.configure(text='The File Does Not Contain All Columns Needed.')
     except:
         if filepath!='':
-            read_wage_lab.configure(text='The file must be an excel.')
+            read_wage_lab.configure(text='The File Must Be An Excel.')
+        else:
+            read_wage_lab.configure(text='The file does not contain Manager Rates and Non-manager Rates.')
 
-def process_file():
-    global df
-    global rates
-    processed_lab.configure(text='')
+def process_shift():
+    '''
+    Done
+    '''
+    global df, PAY_PERIOD
+    shift_processed_lab.configure(text='')
     # Remove parentheses and everything within them
     try:
         df['Service 1 Description (Code)'] = df['Service 1 Description (Code)'].str.replace(r'\(.*\)', '')
         # Remove prefix if it exists
         prefix = 'RC-SDP-CLS-320 '
         df['Service 1 Description (Code)'] = df['Service 1 Description (Code)'].apply(lambda x: x[len(prefix):] if x.startswith(prefix) else x)
+        df['Service 1 Description (Code)'] = df['Service 1 Description (Code)'].apply(lambda x: x.rstrip())
         # Remove everything after " /"
         df['Service Provider'] = df['Service Provider'].str.split(' /', n=1).str[0]
     except:
-        processed_lab.configure(text='Problem Detected in Service 1 Description (Code)/Service Provider')
+        shift_processed_lab.configure(text='Problem Detected in Service 1 Description (Code)/Service Provider')
         return
     # Replace Date/Time with Updated Date/Time if the latter is not NaN
     try:
@@ -94,19 +110,25 @@ def process_file():
         # Create datetime series in Python format
         CIDT = df['Check-In Date'].str.cat(df['Check-In Time'], sep=' ')
         CODT = df['Check-Out Date'].str.cat(df['Check-Out Time'], sep=' ')
-        CIDT = CIDT.apply(lambda x: datetime.strptime(x, r'%m/%d/%Y %I:%M %p'))
-        CODT = CODT.apply(lambda x: datetime.strptime(x, r'%m/%d/%Y %I:%M %p'))
+        CIDT = CIDT.apply(lambda x: datetime.datetime.strptime(x, r'%m/%d/%Y %I:%M %p'))
+        CODT = CODT.apply(lambda x: datetime.datetime.strptime(x, r'%m/%d/%Y %I:%M %p'))
+        df['CIDT'] = CIDT
+        df['CODT'] = CODT
+        PAY_PERIOD = str(CIDT.min().date()) + ' - ' + str(CODT.max().date())
         # Calculate time difference from check-in and check-out datetimes
         CTD = (CODT - CIDT).dt.total_seconds() / 60
+
     except:
-        processed_lab.configure(text='Problem Detected in Check-In/Check-Out Dates and Times')
+        shift_processed_lab.configure(text='Problem Detected in Check-In/Check-Out Dates and Times')
         return
     # Convert Staff Work Duration from Hour:Minutes to Minutes
     try:
         SWD_min = df['Staff Worked Duration'].apply(lambda x: (int(x.split(':')[0]) * 60) + int(x.split(':')[1]))
     except:
-        processed_lab.configure(text='Problem Detected in Staff Worked Duration/Staff Worked Duration (Minutes)')
+        shift_processed_lab.configure(text='Problem Detected in Staff Worked Duration/Staff Worked Duration (Minutes)')
         return
+    
+    df = df.rename(columns={'Service 1 Description (Code)': 'Shift Code'})
     # Error check:
     # 1. Check if Staff Work Duration == Staff Work Duration (Minutes)
     error1 = (SWD_min == df['Staff Worked Duration (Minutes)']) #True means no error
@@ -115,17 +137,55 @@ def process_file():
     df["Passed Error Check"] = (error1 & error2) # The data is "sane" only when both checks are passed
     save_btn["state"] = "normal" # Enable saving botton
     if df['Passed Error Check'].all():
-        processed_lab.configure(text='File Processed. We Found No Errors.')
+        shift_processed_lab.configure(text='File Processed. We Found No Errors.')
     else:
         counts_1 = error1.value_counts()
         counts_2 = error2.value_counts()
         false_count_1 = counts_1[False]
         false_count_2 = counts_2[False]
-        processed_lab.configure(text= f'File Processed. {false_count_1} Staff Work Duration Unequal to Staff Work Duration (Minutes).\n{false_count_2} Staff Work Duration (Minutes) Unequal to Calculated Time Difference.')
+        shift_processed_lab.configure(text= f'Shift File Processed. {false_count_1} Staff Work Duration Unequal to Staff Work Duration (Minutes).\n{false_count_2} Staff Work Duration (Minutes) Unequal to Calculated Time Difference.')
+
+def process_payroll():
+    '''Done'''
+    global df, manager_rates, non_manager_rates, df_aug, non_mgr_payroll, mgr_payroll, PAY_PERIOD
+    df_aug = df.rename(columns={'Service 1 Description (Code)': 'Shift Code'})
+    df_aug = deepcopy(df_aug.loc[~df_aug['Shift Code'].str.contains('Adaptive Behavior Treatment')])
+    # split the 'Service Provider' column on comma separator and extract First name and Surname
+    df_aug[['Surname', 'First Name']] = df_aug['Service Provider'].str.split(', ', expand=True)
+    # concatenate First name and Surname columns in the desired order
+    df_aug['Service Provider'] = df_aug['First Name'] + ' ' + df_aug['Surname']
+    # drop First name and Surname columns
+    df_aug.drop(columns=['First Name', 'Surname'], inplace=True)
+    df_aug=pd.merge(df_aug, non_manager_rates, how='left', on='Shift Code')
+    for name, accrued in zip(manager_rates['Name'], manager_rates['Accrue Rate']):
+        df_aug.loc[(df_aug['Service Provider'] == name), ['Accrue Rate']] = accrued
+    # Add Holiday Hours
+    calc_holiday_hours(df_aug)
+    # Identify staff roles:
+    staff_names = df_aug['Service Provider'].unique()
+    manager_status = [is_manager(i, manager_rates) for i in staff_names]
+    non_mgr = [] #list of names
+    mgr = [] #list of names
+    for i, name in enumerate(staff_names):
+        if manager_status[i]:
+            mgr.append(name)
+        else:
+            non_mgr.append(name)
+    non_mgr_payroll = non_manager_payroll(non_mgr, df_aug, PAY_PERIOD)
+    mgr_payroll = manager_payroll(mgr, manager_rates, df_aug, PAY_PERIOD)
+
+def process_file():
+    '''Done'''
+    global df, manager_rates, non_manager_rates, df_aug, non_mgr_payroll, mgr_payroll, PAY_PERIOD
+    process_shift()
+    process_payroll()
+    df_aug = df_aug.round(decimals=2)
+    non_mgr_payroll = non_mgr_payroll.round(decimals=2)
+    mgr_payroll = mgr_payroll.round(decimals=2)
+
 
 def save_file():
-    global df
-    global rates
+    '''Done'''
     save_lab.configure(text='')
     save_path = asksaveasfile(mode='w', defaultextension=".xlsx").name
     print(save_path)
@@ -133,11 +193,25 @@ def save_file():
         return
     try:
         writer = pd.ExcelWriter(save_path) 
-        df.to_excel(writer, sheet_name='sheet1', index=False, na_rep='NaN')
-        for column in df:
-            column_length = max(df[column].astype(str).map(len).max(), len(column))
-            col_idx = df.columns.get_loc(column)
-            writer.sheets['sheet1'].set_column(col_idx, col_idx, column_length)
+        non_mgr_payroll.to_excel(writer, sheet_name='Non-Manager Payroll', index=False, na_rep='NaN')
+        mgr_payroll.to_excel(writer, sheet_name='Manager Payroll', index=False, na_rep='NaN')
+        df_aug.to_excel(writer, sheet_name='Shift with Pay Rates', index=False, na_rep='NaN')
+        
+        for column in non_mgr_payroll:
+            column_length = max(non_mgr_payroll[column].astype(str).map(len).max(), len(column))
+            col_idx = non_mgr_payroll.columns.get_loc(column)
+            writer.sheets['Non-Manager Payroll'].set_column(col_idx, col_idx, column_length)
+        
+        for column in mgr_payroll:
+            column_length = max(mgr_payroll[column].astype(str).map(len).max(), len(column))
+            col_idx = mgr_payroll.columns.get_loc(column)
+            writer.sheets['Manager Payroll'].set_column(col_idx, col_idx, column_length)
+        
+        for column in df_aug:
+            column_length = max(df_aug[column].astype(str).map(len).max(), len(column))
+            col_idx = df_aug.columns.get_loc(column)
+            writer.sheets['Shift with Pay Rates'].set_column(col_idx, col_idx, column_length)\
+            
         writer.save() 
         save_lab.configure(text='File Saved Successfully!')
     except:
@@ -182,7 +256,7 @@ canvas.create_text(
     54.0,
     69.0,
     anchor="nw",
-    text="Clean shift records, check for errors, and generate payroll.\nBeta version 0.2",
+    text="Clean shift records, check for errors, and generate payroll.\nBeta version 0.3",
     fill="#FFFFFF",
     font=("Inter Bold", 20 * -1)
 )
@@ -258,15 +332,15 @@ read_shift_lab.place(x=696.0,y=277.0)
 read_wage_lab = Label(window, text="", font=('lucida', 20),bg = "white")
 read_wage_lab.place(x=696.0,y=362.0)
 
-processed_lab = Label(window, text="", font=('lucida', 20),bg = "white")
-processed_lab.place(x=696.0,y=550.0)
+shift_processed_lab = Label(window, text="", font=('lucida', 20),bg = "white")
+shift_processed_lab.place(x=696.0,y=550.0)
 
 save_lab = Label(window, text="", font=('lucida', 20), bg = "white")
 save_lab.place(x=696.0, y=740.0)
 
 # Initialize UI  Buttons
 load_shift_btn = Button(window, text = 'upload shift records',command = lambda:open_shift_file(), image=cimg, width=324, height=43,compound='c',font=('lucida', 20))
-load_billing_btn = Button(window, text = 'upload billing & wage rates',image=cimg, width=324, height=43,compound='c',font=('lucida', 20))
+load_billing_btn = Button(window, text = 'upload billing & wage rates',command = lambda:open_rates_file(), image=cimg, width=324, height=43,compound='c',font=('lucida', 20))
 process_btn = Button(window, text = 'process file', command = lambda:process_file(), image=cimg, width=324, height=43,compound='c', font=('lucida', 20))
 save_btn = Button(window, text = 'save as...', state = "disabled", command = lambda: save_file(), image=cimg,width=324, height=43,compound='c',font=('lucida', 20))
 
---
+++ helpers.py
@@ -0,0 +1,108 @@
+import pandas as pd # For importing, manipulating, and exporting data
+import re # Python regular expression support
+import datetime # Python datetime conversion support
+from concurrent.futures import process # Asynchronous callable execution
+from dateutil.easter import *
+import holidays
+from copy import deepcopy
+from collections import namedtuple
+
+Range = namedtuple('Range', ['start', 'end'])
+
+def approved_holiday(YEARS):
+    approved_holiday = set()
+    holiday_set = set(["Thanksgiving", "Christmas Day"])
+    for year in YEARS:
+        ah = {k for k, v in holidays.US(years=year).items() if v in holiday_set}
+        approved_holiday = approved_holiday.union(ah)
+        approved_holiday.add(easter(year))
+        approved_holiday.add(datetime.date(year, 12, 24))
+        approved_holiday.add(datetime.date(year, 12, 31))
+        approved_holiday.add(datetime.date(year-1, 12, 31))
+    return approved_holiday
+
+def approved_holiday_hours(YEARS):
+    approved_holiday_dt = []
+    for x in approved_holiday(YEARS):
+        if (x.month == 12 and x.day == 31):
+            approved_holiday_dt.append(Range(start = datetime.datetime.combine(x, datetime.time(hour=23)),end = datetime.datetime.combine(x+datetime.timedelta(days=1), datetime.time(hour=23))))
+        else:
+            approved_holiday_dt.append(Range(start = datetime.datetime.combine(x, datetime.time(hour=7)), end = datetime.datetime.combine(x, datetime.time(hour=23))))
+    return approved_holiday_dt
+
+def work_holiday_overlap(work_range, ahh_list):
+    total_overlap = 0.0
+    for ahh in ahh_list:
+        latest_start = max(work_range.start, ahh.start)
+        earliest_end = min(work_range.end, ahh.end)
+        delta = (earliest_end - latest_start)
+        overlap = max(0, delta.total_seconds())
+        total_overlap += overlap
+    return total_overlap/60 #in minutes
+
+def calc_holiday_hours(df_payroll):
+        years = set([min([df_payroll['CIDT'][i].year for i in range(len(df_payroll))]), max([df_payroll['CODT'][i].year for i in range(len(df_payroll))])])
+        duration_range = [Range(start = df_payroll['CIDT'][i].to_pydatetime(), end = df_payroll['CODT'][i].to_pydatetime()) for i in range(len(df_payroll))]
+        ahh_list = approved_holiday_hours(years)
+        holiday_hours = [work_holiday_overlap(duration_range[i], ahh_list) for i in range(len(df_payroll))]
+        df_payroll['Holiday Hours'] = holiday_hours
+        
+def is_manager(name, manager_rates):
+    return name in set(manager_rates['Name'])
+
+def manager_is_exempt(name, df_payroll):
+    exempt_mins = df_payroll.loc[(df_payroll['Service Provider'] == name) & (df_payroll['Shift Code'] != 'MGR Direct Care')]['Staff Worked Duration (Minutes)'].sum()
+    non_exempt_mins = df_payroll.loc[(df_payroll['Service Provider'] == name) & (df_payroll['Shift Code'] == 'MGR Direct Care')]['Staff Worked Duration (Minutes)'].sum()
+    return (exempt_mins >= non_exempt_mins)
+
+def worked_overtime(name, df_payroll):
+    return df_payroll.loc[df_payroll['Service Provider'] == name]['Staff Worked Duration (Minutes)'].sum() > (40*60)
+
+def non_manager_payroll(non_mgr, df_payroll, PAY_PERIOD):
+    TGW = [] #Total Gross Wages
+    THW = [] #Total Hours Worked
+    THA = [] #Total Hours Accured
+    for i, name in enumerate(non_mgr):
+        df_indiv = df_payroll.loc[df_payroll['Service Provider'] == name]
+        total_hours_worked = df_indiv['Staff Worked Duration (Minutes)'].sum()/60
+        THW.append(total_hours_worked)
+        THA.append((df_indiv['Staff Worked Duration (Minutes)']/60 * df_indiv['Accrue Rate']).sum())
+        base_salary = (df_indiv['Regular Hourly Wage']*df_indiv['Staff Worked Duration (Minutes)']/60).sum()
+        blended_overtime = 0
+        if worked_overtime(name, df_payroll):
+            BOT_base_rate = (df_indiv['BOT Hourly Wage']*df_indiv['Staff Worked Duration (Minutes)']/60).sum()/total_hours_worked
+            blended_overtime = 0.5 * BOT_base_rate * (total_hours_worked-40)
+        holiday_bonus = 0.5 * (df_indiv['Holiday Hours']*df_indiv['Regular Hourly Wage']).sum() 
+        TGW.append(base_salary + blended_overtime + holiday_bonus)
+    return pd.DataFrame({'Employee Name': non_mgr, 'Pay Peiord': [PAY_PERIOD]*len(THA), 'Total Gross Wages': TGW, 'Total Hours Worked': THW, 'Accured Time Off':THA})
+
+def manager_payroll(mgr, manager_rates, df_payroll, PAY_PERIOD):
+    TGW = [] #Total Gross Wages
+    THW = [] #Total Hours Worked
+    THA = [] #Total Hours Accured
+    Exempt = [] #Exempt status
+    for name in mgr:
+      df_indiv = df_payroll.loc[df_payroll['Service Provider'] == name]
+      total_hours_worked = df_indiv['Staff Worked Duration (Minutes)'].sum()/60
+      exempt_hours_worked = df_indiv.loc[df_indiv['Shift Code'] != 'MGR Direct Care']['Staff Worked Duration (Minutes)'].sum()/60
+      non_exempt_rate = manager_rates.loc[manager_rates['Name'] == name]['Non-exempt Hourly Wage'][0]
+      THW.append(total_hours_worked)
+      THA.append((df_indiv['Staff Worked Duration (Minutes)']/60 * df_indiv['Accrue Rate']).sum())
+      if manager_is_exempt(name, df_payroll):
+        MGR_weekly_salary = manager_rates.loc[manager_rates['Name'] == name]['Exempt Weekly Wage'][0]
+        if name == 'Mikayla Napier':
+          MGR_weekly_salary = manager_rates.loc[manager_rates['Name'] == name]['Exempt Biweekly Wage'][0]
+        non_exempt_salary = (df_indiv['Regular Hourly Wage']*(total_hours_worked - exempt_hours_worked)).sum()
+        holiday_bonus = 0.5 * (df_indiv['Holiday Hours'] * non_exempt_rate).sum()
+        TGW.append(max(MGR_weekly_salary, non_exempt_salary) + holiday_bonus)
+        Exempt.append('Exempt')
+      else: #non-exempt
+        base_salary = (non_exempt_rate * df_indiv['Staff Worked Duration (Minutes)']/60).sum()
+        blended_overtime = 0
+        if worked_overtime(name, df_payroll):
+            BOT_base_rate = non_exempt_rate
+            blended_overtime = 0.5 * BOT_base_rate * (total_hours_worked - 40)
+        holiday_bonus = 0.5 * (df_indiv['Holiday Hours'] * non_exempt_rate).sum()
+        TGW.append(base_salary + blended_overtime + holiday_bonus)
+        Exempt.append('Non-exempt')
+    return pd.DataFrame({'Employee Name': mgr, 'Pay Peiord': [PAY_PERIOD]*len(THA), 'Exempt Status': Exempt, 'Total Gross Wages': TGW, 'Total Hours Worked': THW, 'Accured Time Off':THA})
